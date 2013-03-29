635825580f2c52a7c4b7c84f4cf39ba50b683828
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-03-29 15:39:30 -0700

    portal: create portal.h and move the public api there.
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- portal.c
+++ portal.c
@@ -26,21 +26,8 @@
 #include <linux/init.h>
 #include <linux/poll.h>
 
-/* Public stuff */
-struct umsg {
-	__u32	dst_id;
-	__u32	size;
-	char	data[0];
-};
-
-#define PORTAL_IOC_MAGIC 0x96
-
-enum portal_cmd {
-	PORTAL_MSG_SEND =	_IOWR(PORTAL_IOC_MAGIC, 0x80, struct umsg),
-	PORTAL_MSG_RECV =	_IOWR(PORTAL_IOC_MAGIC, 0x81, struct umsg),
-};
-
-/* End public stuff */
+/* userspace api */
+#include "portal.h"
 
 struct kmsg {
 	struct kref	kref;
