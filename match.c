06557c9d8c11c62335704628c832628cbf41d377
Daniel Mack <zonque@gmail.com>
2013-04-07 15:20:58 +0200

    match: some more parsing bits

--- match.c
+++ match.c
@@ -26,13 +26,29 @@
 
 #include "kdbus_internal.h"
 
+struct kdbus_match_db_entry_item {
+
+	struct list_head	list;
+};
+
+struct kdbus_match_db_entry {
+	u64			id;
+	u64			cookie;
+	u64			src_id;
+	struct list_head	list_entry;
+};
+
 static void __kdbus_match_db_free(struct kref *kref)
 {
+	struct kdbus_match_db_entry *e, *tmp;
 	struct kdbus_match_db *db =
 		container_of(kref, struct kdbus_match_db, kref);
 
 	mutex_lock(&db->entries_lock);
-	// ...
+	list_for_each_entry_safe(e, tmp, &db->entries, list_entry) {
+		list_del(&e->list_entry);
+		kfree(e);
+	}
 	mutex_unlock(&db->entries_lock);
 
 	kfree(db);
@@ -52,8 +68,8 @@ struct kdbus_match_db *kdbus_match_db_new(void)
 		return NULL;
 
 	kref_init(&db->kref);
-	hash_init(db->entries_hash);
 	mutex_init(&db->entries_lock);
+	INIT_LIST_HEAD(&db->entries);
 
 	return db;
 }
@@ -77,15 +93,38 @@ int kdbus_match_db_add(struct kdbus_match_db *db,
 		       void __user *buf)
 {
 	struct kdbus_cmd_match *cmd_match = cmd_match_from_user(buf);
+	struct kdbus_cmd_match_item *item;
+	struct kdbus_match_db_entry *e;
+	u64 size;
+	int ret = 0;
 
 	if (IS_ERR(cmd_match))
 		return PTR_ERR(cmd_match);
 
-	// ...
+	size = cmd_match->size;
+	item = cmd_match->items;
 
+	e = kzalloc(sizeof(*e), GFP_KERNEL);
+	if (!e)
+		return -ENOMEM;
+
+	mutex_lock(&db->entries_lock);
+	INIT_LIST_HEAD(&e->list_entry);
+	e->id = cmd_match->id;
+	e->src_id = cmd_match->src_id;
+	e->cookie = cmd_match->cookie;
+	list_add_tail(&e->list_entry, &db->entries);
+
+	while (size > 0) {
+		// ...
+		size -= item->size;
+		item = (struct kdbus_cmd_match_item *) ((u8 *) item + item->size);
+	}
+
+	mutex_unlock(&db->entries_lock);
 	kfree(cmd_match);
 
-	return 0;
+	return ret;
 }
 
 int kdbus_match_db_remove(struct kdbus_match_db *db,
