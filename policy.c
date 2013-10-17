gregkh/kdbus

commit 48b24f974d131f2d85b47e128cc08f84cfa8db9c

make part iteration generic; *really* validate parts, fix now failing test

policy.c
@@ -226,7 +226,7 @@ static int __kdbus_policy_db_check_send_access(struct kdbus_policy_db *db,
 		}
 	}
 
-	return -EPERM;
+	return 0;
 }
 
 static struct kdbus_policy_db_cache_entry *
@@ -371,13 +371,13 @@ static int kdbus_policy_db_parse(struct kdbus_policy_db *db,
 				 const struct kdbus_cmd_policy *cmd,
 				 u64 size)
 {
-	struct kdbus_policy *pol;
+	const struct kdbus_policy *pol;
 	struct kdbus_policy_db_entry *current_entry = NULL;
 
-	size -= offsetof(struct kdbus_cmd_policy, data);
-	pol = (struct kdbus_policy *) cmd->data;
+	KDBUS_PART_FOREACH(pol, cmd, policies) {
+		if (!KDBUS_PART_VALID(pol, cmd))
+			return -EINVAL;
 
-	while (size > 0) {
 		switch (pol->type) {
 		case KDBUS_POLICY_NAME: {
 			struct kdbus_policy_db_entry *e;
@@ -398,6 +398,7 @@ static int kdbus_policy_db_parse(struct kdbus_policy_db *db,
 			current_entry = e;
 			break;
 		}
+
 		case KDBUS_POLICY_ACCESS: {
 			struct kdbus_policy_db_entry_access *a;
 
@@ -416,16 +417,16 @@ static int kdbus_policy_db_parse(struct kdbus_policy_db *db,
 			mutex_lock(&db->entries_lock);
 			list_add_tail(&a->list, &current_entry->access_list);
 			mutex_unlock(&db->entries_lock);
-
 			break;
 		}
+
 		default:
 			return -EINVAL;
 		}
-
-		size -= pol->size;
-		pol = (struct kdbus_policy *) ((u8 *) pol + pol->size);
 	}
+
+	if (!KDBUS_PART_END(pol, cmd))
+		return -EINVAL;
 
 	return 0;
 }

TODO
@@ -16,6 +16,8 @@ Internal:
     accounts against its budget, security_vm_enough_memory_mm())?
   - how to limit memfd memory?
   - memfd's file pos is shared, which can be confusing, document pread/pwrite
+  - prefix all functions with kdbus
+  - signals miss all the process metadata, we attach only creds
 
 Kernel Core:
   - remove our own task_cgroup_path_from_hierarchy() as soon as it's available:

bus.c
@@ -245,9 +245,8 @@ int kdbus_bus_make_user(void __user *buf, struct kdbus_cmd_bus_kmake **kmake)
 		goto exit;
 	}
 
-	KDBUS_ITEM_FOREACH_VALIDATE(item, &km->make) {
-		/* empty items are invalid */
-		if (item->size <= KDBUS_ITEM_HEADER_SIZE) {
+	KDBUS_PART_FOREACH(item, &km->make, items) {
+		if (!KDBUS_PART_VALID(item, &km->make)) {
 			ret = -EINVAL;
 			goto exit;
 		}
@@ -259,18 +258,18 @@ int kdbus_bus_make_user(void __user *buf, struct kdbus_cmd_bus_kmake **kmake)
 				goto exit;
 			}
 
-			if (item->size < KDBUS_ITEM_HEADER_SIZE + 2) {
+			if (item->size < KDBUS_PART_HEADER_SIZE + 2) {
 				ret = -EINVAL;
 				goto exit;
 			}
 
-			if (item->size > KDBUS_ITEM_HEADER_SIZE + KDBUS_MAKE_MAX_LEN + 1) {
+			if (item->size > KDBUS_PART_HEADER_SIZE + KDBUS_MAKE_MAX_LEN + 1) {
 				ret = -ENAMETOOLONG;
 				goto exit;
 			}
 
 			if (!kdbus_validate_nul(item->str,
-					item->size - KDBUS_ITEM_HEADER_SIZE)) {
+					item->size - KDBUS_PART_HEADER_SIZE)) {
 				ret = -EINVAL;
 				goto exit;
 			}
@@ -279,6 +278,11 @@ int kdbus_bus_make_user(void __user *buf, struct kdbus_cmd_bus_kmake **kmake)
 			continue;
 
 		case KDBUS_MAKE_CGROUP:
+			if (item->size != KDBUS_PART_HEADER_SIZE + sizeof(__u64)) {
+				ret = -EINVAL;
+				goto exit;
+			}
+
 			if (km->cgroup_id) {
 				ret = -EEXIST;
 				goto exit;
@@ -293,8 +297,7 @@ int kdbus_bus_make_user(void __user *buf, struct kdbus_cmd_bus_kmake **kmake)
 		}
 	}
 
-	/* expect correct padding and size values */
-	if ((char *)item - ((char *)&km->make + km->make.size) >= 8)
+	if (!KDBUS_PART_END(item, &km->make))
 		return -EINVAL;
 
 	if (!km->name) {

connection.c
@@ -191,10 +191,10 @@ static int kdbus_conn_payload_add(struct kdbus_conn_queue *queue,
 			return -ENOMEM;
 	}
 
-	KDBUS_ITEM_FOREACH(item, &kmsg->msg) {
+	KDBUS_PART_FOREACH(item, &kmsg->msg, items) {
 		switch (item->type) {
 		case KDBUS_MSG_PAYLOAD_VEC: {
-			size_t size = KDBUS_ITEM_HEADER_SIZE +
+			size_t size = KDBUS_PART_HEADER_SIZE +
 				      sizeof(struct kdbus_vec);
 			char tmp[size];
 			struct kdbus_item *it = (struct kdbus_item *)tmp;
@@ -225,7 +225,7 @@ static int kdbus_conn_payload_add(struct kdbus_conn_queue *queue,
 		}
 
 		case KDBUS_MSG_PAYLOAD_MEMFD: {
-			size_t size = KDBUS_ITEM_HEADER_SIZE +
+			size_t size = KDBUS_PART_HEADER_SIZE +
 				      sizeof(struct kdbus_memfd);
 			char tmp[size];
 			struct kdbus_item *it = (struct kdbus_item *)tmp;
@@ -379,7 +379,7 @@ int kdbus_conn_queue_insert(struct kdbus_conn *conn, struct kdbus_kmsg *kmsg,
 
 	/* add a FDS item; the array content will be updated at RECV time */
 	if (kmsg->fds_count > 0) {
-		size_t size = KDBUS_ITEM_HEADER_SIZE;
+		size_t size = KDBUS_PART_HEADER_SIZE;
 		char tmp[size];
 		struct kdbus_item *it = (struct kdbus_item *)tmp;
 
@@ -525,6 +525,8 @@ int kdbus_conn_kmsg_send(struct kdbus_ep *ep,
 						       kmsg))
 				continue;
 
+			//FIXME: call kdbus_kmsg_append_for_dst()?
+
 			kdbus_conn_queue_insert(conn_dst, kmsg, 0);
 		}
 		mutex_unlock(&ep->bus->lock);
@@ -564,11 +566,9 @@ int kdbus_conn_kmsg_send(struct kdbus_ep *ep,
 			return ret;
 	}
 
-	if (conn_src) {
-		ret = kdbus_kmsg_append_for_dst(kmsg, conn_src, conn_dst);
-		if (ret < 0)
-			return ret;
-	}
+	ret = kdbus_kmsg_append_for_dst(kmsg, conn_src, conn_dst);
+	if (ret < 0)
+		return ret;
 
 	/* monitor connections get all messages */
 	mutex_lock(&ep->bus->lock);
@@ -1127,9 +1127,8 @@ static long kdbus_conn_ioctl_ep(struct file *file, unsigned int cmd,
 			break;
 		}
 
-		KDBUS_ITEM_FOREACH_VALIDATE(item, hello) {
-			/* empty items are invalid */
-			if (item->size <= KDBUS_ITEM_HEADER_SIZE) {
+		KDBUS_PART_FOREACH(item, hello, items) {
+			if (!KDBUS_PART_VALID(item, hello)) {
 				ret = -EINVAL;
 				break;
 			}
@@ -1167,6 +1166,9 @@ static long kdbus_conn_ioctl_ep(struct file *file, unsigned int cmd,
 				ret = -ENOTSUPP;
 			}
 		}
+
+		if (!KDBUS_PART_END(item, hello))
+			return -EINVAL;
 
 		mutex_init(&conn->lock);
 		mutex_init(&conn->names_lock);

endpoint.c
@@ -236,9 +236,8 @@ int kdbus_ep_kmake_user(void __user *buf, struct kdbus_cmd_ep_kmake **kmake)
 		goto exit;
 	}
 
-	KDBUS_ITEM_FOREACH_VALIDATE(item, &km->make) {
-		/* empty items are invalid */
-		if (item->size <= KDBUS_ITEM_HEADER_SIZE) {
+	KDBUS_PART_FOREACH(item, &km->make, items) {
+		if (!KDBUS_PART_VALID(item, &km->make)) {
 			ret = -EINVAL;
 			goto exit;
 		}
@@ -250,18 +249,18 @@ int kdbus_ep_kmake_user(void __user *buf, struct kdbus_cmd_ep_kmake **kmake)
 				goto exit;
 			}
 
-			if (item->size < KDBUS_ITEM_HEADER_SIZE + 2) {
+			if (item->size < KDBUS_PART_HEADER_SIZE + 2) {
 				ret = -EINVAL;
 				goto exit;
 			}
 
-			if (item->size > KDBUS_ITEM_HEADER_SIZE + KDBUS_MAKE_MAX_LEN + 1) {
+			if (item->size > KDBUS_PART_HEADER_SIZE + KDBUS_MAKE_MAX_LEN + 1) {
 				ret = -ENAMETOOLONG;
 				goto exit;
 			}
 
 			if (!kdbus_validate_nul(item->str,
-					item->size - KDBUS_ITEM_HEADER_SIZE)) {
+					item->size - KDBUS_PART_HEADER_SIZE)) {
 				ret = -EINVAL;
 				goto exit;
 			}
@@ -275,9 +274,8 @@ int kdbus_ep_kmake_user(void __user *buf, struct kdbus_cmd_ep_kmake **kmake)
 		}
 	}
 
-	/* expect correct padding and size values */
-	if ((char *)item - ((char *)&km->make + km->make.size) >= 8)
-		return -EINVAL;
+	if (!KDBUS_PART_END(item, &km->make))
+		return EINVAL;
 
 	if (!km->name) {
 		ret = -EBADMSG;

internal.h
@@ -42,22 +42,23 @@
 #define KDBUS_ALIGN8(s) ALIGN((s), 8)
 #define KDBUS_IS_ALIGNED8(s) (IS_ALIGNED(s, 8))
 
-#define KDBUS_ITEM_HEADER_SIZE offsetof(struct kdbus_item, data)
-#define KDBUS_ITEM_SIZE(s) KDBUS_ALIGN8(KDBUS_ITEM_HEADER_SIZE + (s))
-#define KDBUS_ITEM_NEXT(item) \
-	(struct kdbus_item *)((u8 *)item + KDBUS_ALIGN8((item)->size))
-#define KDBUS_ITEM_FOREACH(item, head)						\
-	for (item = (head)->items;						\
-	     (u8 *)(item) < (u8 *)(head) + (head)->size;			\
-	     item = KDBUS_ITEM_NEXT(item))
-/* same iterator with more consistency checks, to be used with incoming data */
-#define KDBUS_ITEM_FOREACH_VALIDATE(item, head)					\
-	for (item = (head)->items;						\
-	     (u8 *)(item) + KDBUS_ITEM_HEADER_SIZE <= (u8 *)(head) + (head)->size && \
-	     (u8 *)(item) + (item)->size <= (u8 *)(head) + (head)->size; \
-	     item = KDBUS_ITEM_NEXT(item))
+#define KDBUS_PART_HEADER_SIZE (2 * sizeof(__u64))
+#define KDBUS_PART_SIZE(s) KDBUS_ALIGN8(KDBUS_PART_HEADER_SIZE + (s))
+#define KDBUS_PART_NEXT(part) \
+	(typeof(part))(((u8 *)part) + KDBUS_ALIGN8((part)->size))
+#define KDBUS_PART_FOREACH(part, head, first)				\
+	for (part = (head)->first;					\
+	     (u8 *)(part) < (u8 *)(head) + (head)->size;		\
+	     part = KDBUS_PART_NEXT(part))
+#define KDBUS_PART_VALID(part, head)					\
+	((part)->size > KDBUS_PART_HEADER_SIZE &&			\
+	 (u8 *)(part) + (part)->size <= (u8 *)(head) + (head)->size)
+#define KDBUS_PART_END(part, head)					\
+	((u8 *)part >= ((u8 *)(head) + (head)->size) &&			\
+	 (u8 *)part - ((u8 *)(head) + (head)->size) < 8)
 
 #define KDBUS_MSG_HEADER_SIZE offsetof(struct kdbus_msg, items)
+#define KDBUS_ITEM_SIZE(s) KDBUS_ALIGN8(KDBUS_PART_HEADER_SIZE + (s))
 
 /* read 64bit .size from struct */
 #define kdbus_size_get_user(_s, _b, _t)						\

kdbus.h
@@ -21,6 +21,18 @@
 #endif
 
 #define KDBUS_IOC_MAGIC			0x95
+#define KDBUS_SRC_ID_KERNEL		(0)
+#define KDBUS_DST_ID_WELL_KNOWN_NAME	(0)
+#define KDBUS_MATCH_SRC_ID_ANY		(~0ULL)
+#define KDBUS_DST_ID_BROADCAST		(~0ULL)
+
+/* Common first elements in a structure which are used to iterate over
+ * a list of elements. */
+#define KDBUS_PART_HEADER \
+	struct {							\
+		__u64 size;						\
+		__u64 type;						\
+	}
 
 /* Message sent from kernel to userspace, when the owner or starter of
  * a well-known name changes */
@@ -60,10 +72,15 @@ struct kdbus_timestamp {
 	__u64 realtime_ns;
 };
 
-#define KDBUS_SRC_ID_KERNEL		(0)
-#define KDBUS_DST_ID_WELL_KNOWN_NAME	(0)
-#define KDBUS_MATCH_SRC_ID_ANY		(~0ULL)
-#define KDBUS_DST_ID_BROADCAST		(~0ULL)
+struct kdbus_vec {
+	__u64 address;
+	__u64 size;
+};
+
+struct kdbus_memfd {
+	__u64 size;
+	int fd;
+};
 
 /* Message Item Types */
 enum {
@@ -100,16 +117,6 @@ enum {
 	KDBUS_MSG_REPLY_DEAD,		/* dito */
 };
 
-struct kdbus_vec {
-	__u64 address;
-	__u64 size;
-};
-
-struct kdbus_memfd {
-	__u64 size;
-	int fd;
-};
-
 /**
  * struct  kdbus_item - chain of data blocks
  *
@@ -117,8 +124,7 @@ struct kdbus_memfd {
  * type: kdbus_item type of data
  */
 struct kdbus_item {
-	__u64 size;
-	__u64 type;
+	KDBUS_PART_HEADER;
 	union {
 		/* inline data */
 		__u8 data[0];
@@ -200,23 +206,25 @@ enum {
 	KDBUS_POLICY_OWN		= 1 <<  0,
 };
 
+struct kdbus_policy_access {
+	__u64 type;		/* USER, GROUP, WORLD */
+	__u64 bits;		/* RECV, SEND, OWN */
+	__u64 id;		/* uid, gid, 0 */
+};
+
+//FIXME: convert access to access[]
 struct kdbus_policy {
-	__u64 size;
-	__u64 type; /* NAME or ACCESS */
+	KDBUS_PART_HEADER;
 	union {
 		char name[0];
-		struct {
-			__u32 type;	/* USER, GROUP, WORLD */
-			__u32 bits;	/* RECV, SEND, OWN */
-			__u64 id;	/* uid, gid, 0 */
-		} access;
+		struct kdbus_policy_access access;
 	};
 };
 
+/* A series of KDBUS_POLICY_NAME, plus one or more KDBUS_POLICY_ACCESS */
 struct kdbus_cmd_policy {
 	__u64 size;
-	__u8 data[0];		/* a series of KDBUS_POLICY_NAME plus one or
-				 * more KDBUS_POLICY_ACCESS each. */
+	struct kdbus_policy policies[0];
 };
 
 /* Flags for struct kdbus_cmd_hello */
@@ -332,10 +340,11 @@ enum {
 	KDBUS_NAME_IN_QUEUE			= 1 << 16,
 };
 
+/* We allow (de)regestration of names of other peers */
 struct kdbus_cmd_name {
 	__u64 size;
-	__u64 name_flags;
-	__u64 id;		/* We allow registration/deregestration of names of other peers */
+	__u64 flags;
+	__u64 id;
 	__u64 conn_flags;
 	char name[0];
 };
@@ -394,7 +403,7 @@ struct kdbus_cmd_monitor {
  *   starter    (via KDBUS_CMD_HELLO with KDBUS_CMD_HELLO_STARTER)
  *   ep owner   (via KDBUS_CMD_EP_MAKE)
  */
-enum kdbus_cmd {
+enum {
 	/* kdbus control node commands: require unset state */
 	KDBUS_CMD_BUS_MAKE =		_IOWR(KDBUS_IOC_MAGIC, 0x00, struct kdbus_cmd_bus_make),
 	KDBUS_CMD_NS_MAKE =		_IOWR(KDBUS_IOC_MAGIC, 0x10, struct kdbus_cmd_ns_make),

match.c
@@ -316,12 +316,12 @@ int kdbus_match_db_add(struct kdbus_conn *conn, void __user *buf)
 	e->src_id = cmd_match->src_id;
 	e->cookie = cmd_match->cookie;
 
-	KDBUS_ITEM_FOREACH_VALIDATE(item, cmd_match) {
+	KDBUS_PART_FOREACH(item, cmd_match, items) {
 		struct kdbus_match_db_entry_item *ei;
 		size_t size;
 
-		if (item->size < sizeof(*item)) {
-			ret = -EBADMSG;
+		if (!KDBUS_PART_VALID(item, cmd_match)) {
+			ret = -EINVAL;
 			break;
 		}
 
@@ -363,6 +363,9 @@ int kdbus_match_db_add(struct kdbus_conn *conn, void __user *buf)
 
 		list_add_tail(&ei->list_entry, &e->items_list);
 	}
+
+	if (!KDBUS_PART_END(item, cmd_match))
+		ret = -EINVAL;
 
 	if (ret >= 0) {
 		list_add_tail(&e->list_entry, &db->entries);

commit a637b70c420bcba97e6905110a8e6b8f085e7d20

policy: fix __kdbus_policy_db_check_send_access return value
This got accidentally broken in commit 48b24f97.

policy.c
@@ -226,7 +226,7 @@ static int __kdbus_policy_db_check_send_access(struct kdbus_policy_db *db,
 		}
 	}
 
-	return 0;
+	return -EPERM;
 }
 
 static struct kdbus_policy_db_cache_entry *