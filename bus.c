83aba4d6db0f39307a3b5b35dad005e805011394
Daniel Mack <zonque@gmail.com>
2013-12-18 01:39:16 +0100

    bus: don't try to free an ERR_PTR

--- bus.c
+++ bus.c
@@ -279,10 +279,8 @@ int kdbus_bus_make_user(void __user *buf, struct kdbus_cmd_make **make,
 		return -EMSGSIZE;
 
 	m = memdup_user(buf, size);
-	if (IS_ERR(m)) {
-		ret = PTR_ERR(m);
-		goto exit;
-	}
+	if (IS_ERR(m))
+		return PTR_ERR(m);
 
 	KDBUS_ITEM_FOREACH(item, m, items) {
 		if (!KDBUS_ITEM_VALID(item, m)) {
