634648472303c1a6db106f8d7370e08028ba0bfd
Daniel Mack <zonque@gmail.com>
2014-03-14 20:29:24 +0100

    metadata: do not try to attach empty connection name

--- metadata.c
+++ metadata.c
@@ -488,7 +488,7 @@ int kdbus_meta_append(struct kdbus_meta *meta,
 	}
 #endif
 
-	if (mask & KDBUS_ATTACH_CONN_NAME && conn) {
+	if ((mask & KDBUS_ATTACH_CONN_NAME) && conn && conn->name) {
 		ret = kdbus_meta_append_str(meta, KDBUS_ITEM_CONN_NAME,
 					    conn->name);
 		if (ret < 0)
