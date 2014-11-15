d32310dd7cd0cce3720e81fec6e0c8c0a386c1be
Daniel Mack <daniel@zonque.org>
2014-11-15 10:16:20 +0100

    Revert "fs: restructure fs_dir_fop_iterate() a little"
    This reverts commit f5a272c36affe1922d64bcfb803f44b26935f885.
    
    This commit introduced a race condition when dir_emit() returns true.
    We have to sync up every time ctx->pos mismatches next->hash.

--- fs.c
+++ fs.c
@@ -102,53 +102,55 @@ static int fs_dir_fop_iterate(struct file *file, struct dir_context *ctx)
 {
 	struct dentry *dentry = file->f_path.dentry;
 	struct kdbus_node *parent = kdbus_node_from_dentry(dentry);
-	struct kdbus_node *next = NULL;
-	struct kdbus_node *prev = file->private_data;
+	struct kdbus_node *old = file->private_data;
+	struct kdbus_node *pos, *next;
 	struct rb_node *rb;
 
 	if (!dir_emit_dots(file, ctx))
 		return 0;
 
-	next = prev;
+	next = old;
 
 	mutex_lock(&parent->lock);
+	do {
+		if (!next || ctx->pos != next->hash) {
+			/* find first node with: hash >= ctx->pos */
+			next = NULL;
+			rb = parent->children.rb_node;
+			while (rb) {
+				pos = kdbus_node_from_rb(rb);
 
-	if (!prev || ctx->pos != prev->hash) {
-		/* find first node with: hash >= ctx->pos */
-		rb = parent->children.rb_node;
-		while (rb) {
-			struct kdbus_node *pos = kdbus_node_from_rb(rb);
-
-			if (ctx->pos < pos->hash) {
-				rb = rb->rb_left;
-				next = pos;
-			} else if (ctx->pos > pos->hash) {
-				rb = rb->rb_right;
-			} else {
-				next = pos;
-				break;
+				if (ctx->pos < pos->hash) {
+					rb = rb->rb_left;
+					next = pos;
+				} else if (ctx->pos > pos->hash) {
+					rb = rb->rb_right;
+				} else {
+					next = pos;
+					break;
+				}
 			}
 		}
-	}
 
-	while (next) {
-		/* skip inactive nodes, and walk down to the next active one */
-		while (!kdbus_node_is_active(next)) {
+		while (next && !kdbus_node_is_active(next)) {
 			rb = rb_next(&next->rb);
-			if (!rb)
-				goto exit_unlock;
-
-			next = kdbus_node_from_rb(rb);
+			if (rb)
+				next = kdbus_node_from_rb(rb);
+			else
+				next = NULL;
 		}
+
+		if (!next)
+			break;
 
 		file->private_data = kdbus_node_ref(next);
 		ctx->pos = next->hash;
 
 		mutex_unlock(&parent->lock);
 
-		/* unref prev entry only if parent mutex is released */
-		kdbus_node_unref(prev);
-		prev = next;
+		/* unref old entry only if parent mutex is released */
+		kdbus_node_unref(old);
+		old = file->private_data;
 
 		if (!dir_emit(ctx, next->name, strlen(next->name), next->id,
 			      kdbus_dt_type(next)))
@@ -157,16 +159,16 @@ static int fs_dir_fop_iterate(struct file *file, struct dir_context *ctx)
 		mutex_lock(&parent->lock);
 
 		rb = rb_next(&next->rb);
-		if (!rb)
-			break;
-
-		next = kdbus_node_from_rb(rb);
-	}
-
-exit_unlock:
+		if (rb) {
+			next = kdbus_node_from_rb(rb);
+			ctx->pos = next->hash;
+		} else {
+			next = NULL;
+		}
+	} while (next);
 	mutex_unlock(&parent->lock);
 
-	kdbus_node_unref(prev);
+	kdbus_node_unref(old);
 	file->private_data = NULL;
 	ctx->pos = INT_MAX;
 
