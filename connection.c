gregkh/kdbus

commit 3f361b67024c685f9aa0bc02f8e52dffbf0a58e2

connection: kdbus_cmd_msg_recv() unlock the conn lock on error paths
If kdbus_cmd_msg_recv() fails make sure to unlock the mutex and avoid
unsafe scenarios.

Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

connection.c
@@ -322,9 +322,10 @@ int kdbus_cmd_msg_recv(struct kdbus_conn *conn,
 		kdbus_pool_slice_free(entry->slice);
 		mutex_unlock(&conn->lock);
 
+		/* Free the resources of this entry */
 		kdbus_queue_entry_free(entry);
 
-		goto exit;
+		goto exit_unlock;
 	}
 
 	/*
@@ -336,7 +337,7 @@ int kdbus_cmd_msg_recv(struct kdbus_conn *conn,
 		recv->dropped_msgs = lost_count;
 		atomic_sub(lost_count, &conn->lost_count);
 		ret = -EOVERFLOW;
-		goto exit;
+		goto exit_unlock;
 	}
 
 	/* Give the offset back to the caller. */
@@ -363,7 +364,6 @@ int kdbus_cmd_msg_recv(struct kdbus_conn *conn,
 
 exit_unlock:
 	mutex_unlock(&conn->lock);
-exit:
 	kdbus_notify_flush(conn->ep->bus);
 	return ret;
 }

commit 964ae2ac6a6c6baf5934655a14436ce66c0bdf53

connection: remove stray mutex_unlock()
Fixes a regression from 3f361b67.

Signed-off-by: Daniel Mack <daniel@zonque.org>

connection.c
@@ -320,7 +320,6 @@ int kdbus_cmd_msg_recv(struct kdbus_conn *conn,
 
 		kdbus_queue_entry_remove(conn, entry);
 		kdbus_pool_slice_free(entry->slice);
-		mutex_unlock(&conn->lock);
 
 		/* Free the resources of this entry */
 		kdbus_queue_entry_free(entry);