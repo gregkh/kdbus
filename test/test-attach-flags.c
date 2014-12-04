72e5abe0820ca92c5de28170da990562649923a6
Djalal Harouni <tixxdz@opendz.org>
2014-12-04 20:31:39 +0100

    test: add the test attach flags source
    Oups add the test-attach-flags source file.
    
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

---
+++ test/test-attach-flags.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <errno.h>
+#include <assert.h>
+#include <sys/types.h>
+
+#include "kdbus-test.h"
+#include "kdbus-util.h"
+#include "kdbus-enum.h"
+
+int kdbus_test_attach_flags(struct kdbus_test_env *env) {
+
+	return TEST_OK;
+}
