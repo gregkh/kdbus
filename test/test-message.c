b9ea61ef0f4ea908a679d54c6a6ecf73313380c2
Djalal Harouni <tixxdz@opendz.org>
2015-01-08 00:25:06 +0100

    test-message: messages that impersonate method replies are rejected
    Signed-off-by: Djalal Harouni <tixxdz@opendz.org>

--- test/test-message.c
+++ test/test-message.c
@@ -81,6 +81,10 @@ int kdbus_test_message_basic(struct kdbus_test_env *env)
 			     0, 0, conn->id);
 	ASSERT_RETURN(ret == -EINVAL);
 
+	/* Faked replies with a valid reply cookie are rejected */
+	ret = kdbus_msg_send_reply(conn, time(NULL) ^ cookie, sender->id);
+	ASSERT_RETURN(ret == -EPERM);
+
 	ret = kdbus_free(conn, offset);
 	ASSERT_RETURN(ret == 0);
 
