5663307126c4bcca267d240bd084c02b7129f012
Lukasz Skalski <l.skalski@samsung.com>
2015-02-11 14:33:34 +0100

    test: check what happens if we forget about KDBUS_CMD_FREE call

--- test/test-free.c
+++ test/test-free.c
@@ -14,6 +14,22 @@
 #include "kdbus-enum.h"
 #include "kdbus-test.h"
 
+static int sample_ioctl_call(struct kdbus_test_env *env)
+{
+	int ret;
+	struct kdbus_cmd_list cmd_list = {
+		.flags = KDBUS_LIST_QUEUED,
+		.size = sizeof(cmd_list),
+	};
+
+	ret = kdbus_cmd_list(env->conn->fd, &cmd_list);
+	ASSERT_RETURN(ret == 0);
+
+	/* DON'T FREE THIS SLICE OF MEMORY! */
+
+	return TEST_OK;
+}
+
 int kdbus_test_free(struct kdbus_test_env *env)
 {
 	int ret;
@@ -31,6 +47,18 @@ int kdbus_test_free(struct kdbus_test_env *env)
 	cmd_free.offset = POOL_SIZE + 1;
 	ret = kdbus_cmd_free(env->conn->fd, &cmd_free);
 	ASSERT_RETURN(ret == -ENXIO);
+
+	/*
+	 * The user application is responsible for freeing the allocated
+	 * memory with the KDBUS_CMD_FREE ioctl, so let's test what happens
+	 * if we forget about it.
+	 */
+
+	ret = sample_ioctl_call(env);
+	ASSERT_RETURN(ret == 0);
+
+	ret = sample_ioctl_call(env);
+	ASSERT_RETURN(ret == 0);
 
 	return TEST_OK;
 }
